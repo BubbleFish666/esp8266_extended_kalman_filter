ff028fec9b43c87e4eecd3ec16693495ef683d5d
Bubble Fish <bubblefish666@outlook.com>
2023-11-12 17:22:04 +0100

    rename variables

--- include/extended_kalman_filter.h
+++ include/extended_kalman_filter.h
@@ -3,38 +3,38 @@
 class ExtendedKalmanFilter {
 private:
   // process noise covariance Q
-  Eigen::Matrix3f _Q;
+  Eigen::Matrix3f Q_;
 
   // measurement noise covariance R
-  Eigen::Matrix3f _R;
+  Eigen::Matrix3f R_;
 
   // prediction
   // state estimate of last step
-  Eigen::Vector3f _x_c_k_1;
+  Eigen::Vector3f x_c_k_1_;
   // estimate error covariance of last step
-  Eigen::Matrix3f _P_k_1;
+  Eigen::Matrix3f P_k_1_;
   // state Jacobian
-  Eigen::Matrix3f _A_k;
+  Eigen::Matrix3f A_k_;
 
   // correction
   // predicted state
-  Eigen::Vector3f _x_p_k;
+  Eigen::Vector3f x_p_k_;
   // predicted measurement
-  Eigen::Vector3f _z_p_k;
+  Eigen::Vector3f z_p_k_;
   // predicted estimate error covariance
-  Eigen::Matrix3f _P_p_k;
+  Eigen::Matrix3f P_p_k_;
   // measurement Jacobian
-  Eigen::Matrix3f _H_k;
+  Eigen::Matrix3f H_k_;
   // Kalman gain
-  Eigen::Matrix3f _K_k;
+  Eigen::Matrix3f K_k_;
   // measurement
-  Eigen::Vector3f _z_k;
+  Eigen::Vector3f z_k_;
 
   // estimate (output of correction)
   // state estimate
-  Eigen::Vector3f _x_c_k;
+  Eigen::Vector3f x_c_k_;
   // estimate error covariance
-  Eigen::Matrix3f _P_k;
+  Eigen::Matrix3f P_k_;
 
   void stateFcn();
   void stateJacFcn();
--- src/extended_kalman_filter.cpp
+++ src/extended_kalman_filter.cpp
@@ -5,43 +5,43 @@
   @param x_0 initial state
 */
 ExtendedKalmanFilter::ExtendedKalmanFilter(Eigen::Vector3f x_0)
-    : _x_c_k_1(x_0) {
+    : x_c_k_1_(x_0) {
   // process noise covariance
-  _Q << 0.5, 0, 0.01,
+  Q_ << 0.5, 0, 0.01,
         0, 0.5, 0.01,
         0.01, 0.01, 0.3;
   // measurement noise covariance
-  _R << 0.5, 0, 0.1,
+  R_ << 0.5, 0, 0.1,
         0, 0.5, 0.1,
         0.1, 0.1, 0.6;
   // initial estimate error covariance
-  _P_k_1 = Eigen::Matrix3f::Identity();
+  P_k_1_ = Eigen::Matrix3f::Identity();
 }
 
 /*state function of the system*/
 void ExtendedKalmanFilter::stateFcn() {
-  _x_p_k(0) = _x_c_k_1(0);
-  _x_p_k(1) = _x_c_k_1(1);
-  _x_p_k(2) = std::atan2(_x_c_k_1(1), _x_c_k_1(0));
+  x_p_k_(0) = x_c_k_1_(0);
+  x_p_k_(1) = x_c_k_1_(1);
+  x_p_k_(2) = std::atan2(x_c_k_1_(1), x_c_k_1_(0));
 }
 
 /*Jacobian of the state function of the system*/
 void ExtendedKalmanFilter::stateJacFcn() {
-  _A_k << 1, 0, 0,
+  A_k_ << 1, 0, 0,
           0, 1, 0,
-          -_x_c_k_1(1)/(_x_c_k_1(0)*_x_c_k_1(0) + _x_c_k_1(1)*_x_c_k_1(1)),
-          _x_c_k_1(0)/(_x_c_k_1(0)*_x_c_k_1(0) + _x_c_k_1(1)*_x_c_k_1(1)),
+          - x_c_k_1_(1) / (x_c_k_1_(0) * x_c_k_1_(0) + x_c_k_1_(1) * x_c_k_1_(1)),
+          x_c_k_1_(0) / (x_c_k_1_(0) * x_c_k_1_(0) + x_c_k_1_(1) * x_c_k_1_(1)),
           0;
 }
 
 /*measurement function of the system*/
 void ExtendedKalmanFilter::measureFcn() {
-  _z_p_k = _x_p_k;
+  z_p_k_ = x_p_k_;
 }
 
 /*Jacobian of the measurement function of the system*/
 void ExtendedKalmanFilter::measureJacFcn() {
-  _H_k = Eigen::Matrix3f::Identity();
+  H_k_ = Eigen::Matrix3f::Identity();
 }
 
 /*do prediction*/
@@ -51,7 +51,7 @@ void ExtendedKalmanFilter::predict() {
   // calculate _A_k
   stateJacFcn();
   // calculate _P_p_k
-  _P_p_k = _A_k * _P_k_1 * _A_k.transpose() + _Q;
+  P_p_k_ = A_k_ * P_k_1_ * A_k_.transpose() + Q_;
 }
 
 /*do correction*/
@@ -59,14 +59,14 @@ void ExtendedKalmanFilter::correct() {
   // calculate _H_k
   measureJacFcn();
   // calculate _K_k
-  _K_k = _P_p_k * _H_k.transpose() *
-         (_H_k * _P_p_k * _H_k.transpose() + _R).inverse();
+  K_k_ = P_p_k_ * H_k_.transpose() *
+         (H_k_ * P_p_k_ * H_k_.transpose() + R_).inverse();
   // calculate _z_p_k
   measureFcn();
   // calculate _x_c_k
-  _x_c_k = _x_p_k + _K_k * (_z_k - _z_p_k);
+  x_c_k_ = x_p_k_ + K_k_ * (z_k_ - z_p_k_);
   // calculate _P_k
-  _P_k = (Eigen::Matrix3f::Identity() - _K_k * _H_k) * _P_p_k;
+  P_k_ = (Eigen::Matrix3f::Identity() - K_k_ * H_k_) * P_p_k_;
 }
 
 /*do estimation for the current step
@@ -74,10 +74,10 @@ void ExtendedKalmanFilter::correct() {
   @returns estimate of state
 */
 Eigen::Vector3f ExtendedKalmanFilter::estimate(Eigen::Vector3f z_k) {
-  _z_k = z_k;
+  z_k_ = z_k;
   predict();
   correct();
-  _x_c_k_1 = _x_c_k;
-  _P_k_1 = _P_k;
-  return _x_c_k;
+  x_c_k_1_ = x_c_k_;
+  P_k_1_ = P_k_;
+  return x_c_k_;
 }
